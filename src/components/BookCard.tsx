06e48843f173ef23ebe727971d1e98cde513798e
asann3 <39186984+asann3@users.noreply.github.com>
2026-03-17 19:10:53 +0900

    feat: Show no-image.svg when thumbnailUrl is null

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -5,8 +5,11 @@ interface BookCardProps {
 }
 
 export function BookCard({ book }: BookCardProps) {
+  const imgSrc = book.thumbnailUrl ?? '/no-image.svg'
+
   return (
     <div>
+      <img src={imgSrc} alt={book.title} />
       <div>{book.title}</div>
       {book.author && <div>{book.author}</div>}
       {book.publisher && <div>{book.publisher}</div>}
